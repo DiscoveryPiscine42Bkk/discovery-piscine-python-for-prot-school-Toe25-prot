5fcf12a669e0aa99d0374c9f1fa91d2d39104900
Toe25-prot <toeboyfc@gmail.com>
2025-03-10 15:15:01 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+number = int(input("please enter your number"))
+
+if number < 0 :
+    print("This is negative")
+if number == 0 :
+    print("this number is both side negative and posiive")
+if number > 0 :
+    print("this number is positive")
+
