09ea96a938e9d8328fb82859d6c2eb2c4f2160f3
Toe25-prot <toeboyfc@gmail.com>
2025-03-11 10:27:04 +0700

    Add files via upload

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,8 @@
+nu = input("what you gotta say? : ")
+
+while True:
+    response = input("I got that!,Anything else? : ")
+    if response.lower() == "stop":
+        print("Program stopped.")
+        break
+    
