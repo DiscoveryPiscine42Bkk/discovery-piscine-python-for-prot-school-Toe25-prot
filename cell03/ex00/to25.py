a76e7213b197ef6a4a036af407ad9b352589c932
Toe25-prot <toeboyfc@gmail.com>
2025-03-11 09:49:03 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,13 @@
+number_2 = int(input("Enter the number less than 25 : "))
+
+if number_2 <= 25 :
+  
+
+ 
+ while number_2 <= 25:
+    print("inside the loop , my variable",number_2 )
+    number_2 += 1 
+
+else :
+  print("error")
+  
