6a5d78bccd303346b595c964fb938b55df543aeb
Toe25-prot <toeboyfc@gmail.com>
2025-03-11 11:10:29 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,9 @@
+nm = int(input("Give me your first number : "))
+pd = int(input("give me your second number : "))
+
+
+print("Thank you!")
+print(nm,"+",pd,"=",nm + pd)
+print(nm,"-",pd,"=",nm-pd)
+print(nm,"/",pd,"=",nm/pd)
+print(nm,"*",pd,"=",nm*pd)
