9152b7cf9f858f502f492d36ae889134c0b67712
Toe25-prot <toeboyfc@gmail.com>
2025-03-11 09:26:32 +0700

    Add files via upload

--- cell02/ex03/mult.py
+++ cell02/ex03/mult.py
@@ -1,8 +1,9 @@
 number_1 = int(input("enter  the first number"))
 number_2 = int(input("enter the second number"))
 
-result = number_1 * number_2 
- 
+result = number_1 * number_2
+print(number_1,"x",number_2,result)
+
 if result < 0 : 
     print("the resilt is negative")
 elif result > 0 :
